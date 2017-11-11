347796bd51a98a7906ebe1ef729309557e066ae4
Kaleb Davenport <kaleb@wowyourclient.com>
2017-11-11 14:27:44 -0500

    Add files via upload

---
+++ exampleFunction.js
@@ -0,0 +1,16 @@
+function reusableFunction(input_container) {
+    // Do something for each input_container
+}
+
+reusableFunction("actual_input_1");
+reusableFunction("actual_input_2");
+
+
+// Example
+function welcomeUser(user_name) {
+    console.log("My name is" + user_name);
+}
+
+welcomeUser("Kaleb");
+welcomeUser("Ashten");
+welcomeUser("Jewels");
